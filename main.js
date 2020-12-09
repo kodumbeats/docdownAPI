d6bddcd4cd63e9d0905d31c1dd48089abb0ce6e2
kodumbeats <brandon.leckemby@gmail.com>
2020-12-09 14:47:33 -0500

    added error handling and promise catches

--- main.js
+++ main.js
@@ -19,7 +19,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
-app.post("/render", (req, res) => {
+app.post("/render", async (req, res) => {
   function findRoot(indexLog) {
     const entries = Object.entries(indexLog);
     const found = entries.findIndex((e) => e[1] == 0);
@@ -37,108 +37,126 @@ app.post("/render", (req, res) => {
   };
   const priority = ["h1", "h2", "h3", "h4", "p"];
   let prepend;
-  ast.forEach((line) => {
-    // define actions for
-    switch (line.name) {
-      case "h1":
-        indexLog.h1 = indexLog.h1 + 1;
-        prepend = indexLog.h1;
-        line.children[0].content = prepend + " " + line.children[0].content;
-        //TODO@kodumbeats: get rid of this variable reset (fix?: add priority order matching, requires finding the first zero in indexLog)
-        indexLog.h2 = 0;
-        indexLog.h3 = 0;
-        indexLog.h4 = 0;
-        indexLog.p = 0;
-        indexLog.img = 0;
-        break;
-      case "h2":
-        indexLog.h2 = indexLog.h2 + 1;
-        prepend = indexLog.h1 + "." + indexLog.h2;
-        line.children[0].content = prepend + " " + line.children[0].content;
-        indexLog.h3 = 0;
-        indexLog.h4 = 0;
-        indexLog.p = 0;
-        indexLog.img = 0;
-        break;
-      case "h3":
-        indexLog.h3 = indexLog.h3 + 1;
-        prepend = indexLog.h1 + "." + indexLog.h2 + "." + indexLog.h3;
-        line.children[0].content = prepend + " " + line.children[0].content;
-        indexLog.h4 = 0;
-        indexLog.p = 0;
-        indexLog.img = 0;
-        break;
-      case "h4":
-        indexLog.h4 = indexLog.h4 + 1;
-        prepend =
-          indexLog.h1 +
-          "." +
-          indexLog.h2 +
-          "." +
-          indexLog.h3 +
-          "." +
-          indexLog.h4;
-        line.children[0].content = prepend + " " + line.children[0].content;
-        indexLog.p = 0;
-        indexLog.img = 0;
-        break;
-      case "p":
-        indexLog.p = indexLog.p + 1;
-        const r = findRoot(indexLog);
-        switch (r) {
-          case "h1":
-            prepend = indexLog.h1 + "." + indexLog.p;
+  try {
+    ast.forEach((line) => {
+      // define actions for
+      switch (line.name) {
+        case "h1":
+          indexLog.h1 = indexLog.h1 + 1;
+          prepend = indexLog.h1;
+          if (line.children[0]) {
             line.children[0].content = prepend + " " + line.children[0].content;
-            break;
-          case "h2":
-            prepend = indexLog.h1 + "." + indexLog.h2 + "." + indexLog.p;
-            line.children[0].content = prepend + " " + line.children[0].content;
-            break;
-          case "h3":
-            prepend =
-              indexLog.h1 +
-              "." +
-              indexLog.h2 +
-              "." +
-              indexLog.h3 +
-              "." +
-              indexLog.p;
-            line.children[0].content = prepend + " " + line.children[0].content;
-            break;
-          case "h4":
-            prepend =
-              indexLog.h1 +
-              "." +
-              indexLog.h2 +
-              "." +
-              indexLog.h3 +
-              "." +
-              indexLog.h4 +
-              "." +
-              indexLog.p;
-            line.children[0].content = prepend + " " + line.children[0].content;
-            break;
-          default:
-            console.log("EOF");
-        }
-        break;
-      case undefined:
-        if (line.content === "\n") {
-          //newlines fail the switch - no name field
+          }
+          //TODO@kodumbeats: get rid of this variable reset (fix?: add priority order matching, requires finding the first zero in indexLog)
+          indexLog.h2 = 0;
+          indexLog.h3 = 0;
+          indexLog.h4 = 0;
+          indexLog.p = 0;
+          indexLog.img = 0;
           break;
-        } else {
-          console.warn({
-            warn: "undefined line",
-            line,
-          });
+        case "h2":
+          indexLog.h2 = indexLog.h2 + 1;
+          prepend = indexLog.h1 + "." + indexLog.h2;
+          if (line.children[0]) {
+            line.children[0].content = prepend + " " + line.children[0].content;
+          }
+          indexLog.h3 = 0;
+          indexLog.h4 = 0;
+          indexLog.p = 0;
+          indexLog.img = 0;
           break;
-        }
-      default:
-        console.log(`No case for ${line.name}`);
-        break;
-    }
-  });
-  console.log(frontmatter);
+        case "h3":
+          indexLog.h3 = indexLog.h3 + 1;
+          prepend = indexLog.h1 + "." + indexLog.h2 + "." + indexLog.h3;
+          if (line.children[0]) {
+            line.children[0].content = prepend + " " + line.children[0].content;
+          }
+          indexLog.h4 = 0;
+          indexLog.p = 0;
+          indexLog.img = 0;
+          break;
+        case "h4":
+          indexLog.h4 = indexLog.h4 + 1;
+          prepend =
+            indexLog.h1 +
+            "." +
+            indexLog.h2 +
+            "." +
+            indexLog.h3 +
+            "." +
+            indexLog.h4;
+          if (line.children[0]) {
+            line.children[0].content = prepend + " " + line.children[0].content;
+          }
+          indexLog.p = 0;
+          indexLog.img = 0;
+          break;
+        case "p":
+          indexLog.p = indexLog.p + 1;
+          const r = findRoot(indexLog);
+          switch (r) {
+            case "h1":
+              prepend = indexLog.h1 + "." + indexLog.p;
+              line.children[0].content =
+                prepend + " " + line.children[0].content;
+              break;
+            case "h2":
+              prepend = indexLog.h1 + "." + indexLog.h2 + "." + indexLog.p;
+              line.children[0].content =
+                prepend + " " + line.children[0].content;
+              break;
+            case "h3":
+              prepend =
+                indexLog.h1 +
+                "." +
+                indexLog.h2 +
+                "." +
+                indexLog.h3 +
+                "." +
+                indexLog.p;
+              line.children[0].content =
+                prepend + " " + line.children[0].content;
+              break;
+            case "h4":
+              prepend =
+                indexLog.h1 +
+                "." +
+                indexLog.h2 +
+                "." +
+                indexLog.h3 +
+                "." +
+                indexLog.h4 +
+                "." +
+                indexLog.p;
+              line.children[0].content =
+                prepend + " " + line.children[0].content;
+              break;
+            default:
+              console.log("EOF");
+          }
+          break;
+        case undefined:
+          if (line.content) {
+            break;
+          }
+          if (line.content === "\n") {
+            //newlines fail the switch - no name field
+            break;
+          } else {
+            console.warn({
+              warn: "undefined line",
+              line,
+            });
+            break;
+          }
+        default:
+          console.log(`No case for ${line.name}`);
+          break;
+      }
+    });
+  } catch (err) {
+    console.log({ err });
+  }
   // parse frontmatter
   const yamlObj = yaml.safeLoad(frontmatter);
 
@@ -155,6 +173,7 @@ app.post("/render", (req, res) => {
         <tr><th>${yamlObj.docnum}</th><th>${yamlObj.title}</th><th>${yamlObj.eff}</th></tr>
       </table>
     </div>`;
+
   const payload = {
     header: header,
     data: HTML.stringify(ast),
